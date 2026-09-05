00f3012181982b7ee029dec466bccf0f12e5cb60
VijaySingh092 <vijaykhetwal372@gmail.com>
2026-09-05 20:02:11 +0530

    captain register

--- Backend/app.js
+++ Backend/app.js
@@ -6,7 +6,7 @@ const app = express()
 const connectToDb = require('./db/db')
 const cookieParser = require('cookie-parser')
 const userRoutes = require('./routes/user.routes')
-
+const captainRoutes = require('./routes/captain.routes')
 app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
@@ -17,6 +17,7 @@ app.get('/',(req,res)=>{
 })
 
 app.use('/users',userRoutes)
+app.use('/captains',captainRoutes)
 
 connectToDb()
 
