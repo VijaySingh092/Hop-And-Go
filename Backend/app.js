cddd026e691e05852f76c53bf8d58f7b3a971c1b
VijaySingh092 <vijaykhetwal372@gmail.com>
2026-09-05 10:51:13 +0530

    user registration

--- Backend/app.js
+++ Backend/app.js
@@ -2,13 +2,20 @@ const dotenv= require('dotenv')
 dotenv.config()
 const express = require('express')
 const cors = require('cors')
-
 const app = express()
+const connectToDb = require('./db/db')
+const userRoutes = require('./routes/user.routes')
 
 app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 app.get('/',(req,res)=>{
     res.send("Hello World")
 })
+
+app.use('/users',userRoutes)
+
+connectToDb()
 
 module.exports= app
