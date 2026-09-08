ad3d6175fc4bb7eee192aa3cdb379ea241464e61
VijaySingh092 <vijaykhetwal372@gmail.com>
2026-09-08 11:52:34 +0530

    user home frontend

---
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LocationSearchPanel = () => {
+  return (
+    <div>
+      locationSearchPanel
+    </div>
+  )
+}
+
+export default LocationSearchPanel
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,92 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import gsap from 'gsap'
+import logo from '../assets/hop-go-logo-png_seeklogo-67856-removebg-preview.png'
+import {useGSAP} from '@gsap/react'
+import 'remixicon/fonts/remixicon.css'
+import LocationSearchPanel from '../components/LocationSearchPanel'
+
 
 const Home = () => {
+  const [pickup,setPickup]=useState('')
+  const [destination,setDestination] = useState('')
+  const [panelOpen,setPanelOpen] =useState(false)
+  const panelRef=useRef(null)
+  const panelCloseRef = useRef(null)
+
+  const submitHandler=(e)=>{
+    e.preventDefault()
+  }
+
+  useGSAP(function(){
+    if(panelOpen){
+      gsap.to(panelRef.current,{
+    height:'70%',
+    padding:24
+    
+    })
+    gsap.to(panelCloseRef.current,{
+      opacity:1
+    })
+    }else{
+      gsap.to(panelRef.current,{
+    height:'0%',
+    padding:0
+    
+    })
+    gsap.to(panelCloseRef.current,{
+      opacity:0
+    })
+    }
+  },[panelOpen])
+
   return (
-    <div>
-      HOme
+    <div className='h-screen relative'>
+      <img className='w-16 left-5 top-5 absolute' src={logo} alt="" />
+
+      <div className='h-screen w-screen'>
+        <img className='h-full w-full object-cover' src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="" />
+        </div>
+        <div className=' flex flex-col justify-end h-screen absolute top-0 w-full'>
+          <div className='h-[30%] bg-white p-6 relative'>
+            <h5 ref={panelCloseRef} onClick={()=>{
+              setPanelOpen(false)
+            }} className='absolute opacity-0 top-6 right-6 text-2xl'>
+              <i className="ri-arrow-down-wide-line"></i>
+              </h5>
+            <h4 className='text-2xl font-semibold '>Find a trip</h4>
+          <form onSubmit={(e)=>{
+            submitHandler(e)
+          }}> 
+          <div className='line absolute h-16 w-1 top-[45%] left-10 bg-gray-900 rounded-full'></div>
+            <input
+            onClick={()=>{
+              setPanelOpen(true)
+            }}
+            value={pickup}
+            onChange={(e)=>{
+              setPickup(e.target.value)
+            }} 
+            className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full mt-5' 
+            type="text" 
+            placeholder='Add a pickup location' />
+            <input 
+            onClick={()=>{
+              setPanelOpen(true)
+            }}
+            value={destination}
+            onChange={(e)=>{
+              setDestination(e.target.value)
+            }} 
+            className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full mt-3' 
+            type="text" 
+            placeholder='Enter your destination' />
+          </form>
+          </div>
+          <div ref={panelRef} className='h-0  bg-red-500 '>
+            <LocationSearchPanel/>
+          </div>
+        </div>
+    
     </div>
   )
 }
